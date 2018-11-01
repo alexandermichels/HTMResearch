ab4b66cf423bdede7fa40838f40dc8cae6504d0b
alexandermichels <alexandercm4297@gmail.com>
2018-10-31 23:07:43 -0400

    Working on sequences

--- src/main/models/SimpleSequence.py
+++ src/main/models/SimpleSequence.py
@@ -1,27 +1,53 @@
 from matplotlib import pyplot
 import numpy as np
+from random import randint, random
 
 from Sequence import Sequence
 
-class SimpleSequence(Sequence):
+class OneTermSimpleSequence(Sequence):
 
-    def __init__(self, order, lags, determinism=1.0, n=10000):
+    def __init__(self, order, lag, determinism=1.0, n=10000):
         """
         Creates a sequence object with a lag-ary operation defining transitions from each othe order elements to the other order.
 
         :param order: the number of elements that may appear in the sequence.
         :param lags: a binary array
         """
-        self.n = n
         self.theta = 0
         self.order = order
-        self.dim = np.sum(lags, dtype=np.int32)
-        
+        self.lag = lag
+        self.determinism = determinism
+        self.n = n
+        self.sequence = np.ones(n)
+
+        #make the transition matrix
+        self.transition = np.ones(order)
+        for i in range(len(self.transition)):
+            if random() < self.determinism:
+                self.transition[i] = randint(1, self.order)
+            else:
+                self.transition[i] = 0
+
+        #seed the sequence
+        counter = 0
+        while counter < self.lag:
+            self.sequence[counter] = randint(1, self.order)
+            counter+=1
+        while counter < self.n:
+            where_to = int(self.sequence[counter-self.lag]-1)
+            print(where_to)
+            next = self.transition[where_to]
+            if next == 0:
+                self.sequence[counter] = randint(1, self.order)
+            else:
+                self.sequence[counter] = next
+            counter+=1
+
 
 
 def main():
-    ts = SimpleSequence()
-    for i in range(10):
+    ts = OneTermSimpleSequence(7,3)
+    for i in range(100):
         print("The time series at {} is {}".format(i, ts.get()))
     ts.plot()
 
